9573be5e669322aaa657a97c8793d4ea53ef3f18
Bernardo Antunes <bernasss12@gmail.com>
2024-04-23 18:09:06 +0100

    Fix/Reimplement enchantment glint

--- src/main/java/dev/bernasss12/bebooks/mixin/ItemStackMixin.java
+++ src/main/java/dev/bernasss12/bebooks/mixin/ItemStackMixin.java
@@ -2,41 +2,52 @@ package dev.bernasss12.bebooks.mixin;
 
 import java.util.List;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.text.Text;
 
 import dev.bernasss12.bebooks.config.ModConfig;
 import dev.bernasss12.bebooks.manage.MaxEnchantmentManager;
-import dev.bernasss12.bebooks.util.NBTUtil;
 import dev.bernasss12.bebooks.util.Util;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ItemStack.class, priority = 10)
 @Environment(EnvType.CLIENT)
 public abstract class ItemStackMixin {
 
-    @ModifyVariable(
-            method = "appendEnchantments",
-            argsOnly = true,
-            at = @At("HEAD")
-    )
-    private static NbtList appendEnchantmentHead(NbtList tag, List<Text> tooltip, NbtList enchantments) {
-        if (MinecraftClient.getInstance().currentScreen instanceof HandledScreen) {
-            return NBTUtil.sorted(enchantments, ModConfig.INSTANCE.getSortingMode(), ModConfig.INSTANCE.getKeepCursesBelow());
+    //FIXME find new way to sort enchantments in tooltip.
+
+    //    @ModifyVariable(
+    //            method = "appendEnchantments",
+    //            argsOnly = true,
+    //            at = @At("HEAD")
+    //    )
+    //    private static NbtList appendEnchantmentHead(NbtList tag, List<Text> tooltip, NbtList enchantments) {
+    //        if (MinecraftClient.getInstance().currentScreen instanceof HandledScreen) {
+    //            return NBTUtil.sorted(enchantments, ModConfig.INSTANCE.getSortingMode(), ModConfig.INSTANCE.getKeepCursesBelow());
+    //        }
+    //        return tag;
+    //    }
+
+    @Shadow
+    public abstract Item getItem();
+
+    @Inject(method = "hasGlint", at = @At(value = "HEAD"), cancellable = true)
+    protected void changeEnchantedBooksGlintReturn(CallbackInfoReturnable<Boolean> info) {
+        if (!ModConfig.INSTANCE.getEnchantedBookGlint() && this.getItem() == Items.ENCHANTED_BOOK) {
+            info.setReturnValue(false);
         }
-        return tag;
     }
 
 //    @Dynamic("ItemStack.appendEnchantments' lambda")
